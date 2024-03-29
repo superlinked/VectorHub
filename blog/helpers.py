b67349fd760ab60644752784a534ccd99850c84b
Chirag Jain <jainchg@gmail.com>
2024-03-30 01:40:17 +0530

    add scripts to upload blogs to Strapi

--- blog/helpers.py
+++ blog/helpers.py
@@ -1,16 +1,30 @@
+import os
 import enum
-import json
+
+GIT_REPO = 'https://github.com/superlinked/VectorHub'
 
 class ItemType(enum.Enum):
     FOLDER = "folder"
     FILE = "file"
 
 class Item:
-    def __init__(self, type, name, path, children):
+    def __init__(self, type, name, path, has_blogs=False, children=None):
         self.type = type
         self.name = name
         self.path = path
-        self.children = children
+        self.has_blogs = has_blogs
+        self.children = []
+
+        if children:
+            self.add_children(children)
+
+    def __str__(self) -> str:
+        return self.path
+
+    def add_children(self, children):
+        for child in children:
+            # Recursively create DirectoryItem objects for children
+            self.children.append(Item.from_dict(child))
 
     @classmethod
     def from_dict(cls, data):
@@ -18,9 +32,10 @@ class Item:
         Create an Item instance from a dictionary.
         """
         return cls(
-            type=data.get("type", ItemType.FILE),
+            type=data.get("type", ItemType.FOLDER),
             name=data.get("name", ""),
             path=data.get("path", ""),
+            has_blogs=data.get("has_blogs", False),
             children=data.get("children", [])
         )
 
@@ -32,34 +47,48 @@ class Item:
             "type": self.type,
             "path": self.path,
             "name": self.name,
+            "has_blogs": self.has_blogs,
             "children": self.children,
         }
 
 
-class Items:
-    def __init__(self):
-        self.items = []
+class StrapiBlogType(enum.Enum):
+    USECASE = "USECASE"
+    ARTICLE = "ARTICLE"
 
-    def load_from_file(self, filename):
-        """
-        Load item data from a JSON file.
-        """
-        with open(filename, 'r') as file:
-            data = json.load(file)
-            for item_data in data:
-                self.items.append(Item.from_dict(item_data))
 
-    def add_item(self, user):
-        """
-        Add an Item object to the list.
-        """
-        if isinstance(user, Item):
-            self.items.append(iter)
-        else:
-            raise TypeError("item must be an instance of Item")
+class StrapiBlog:
+    def __init__(self, content, filepath, last_updated, type: StrapiBlogType):
+        self.content = content
+        self.filepath = filepath
+        self.last_updated = last_updated
+        self.type = type
 
-    def to_json(self):
-        """
-        Convert the Items object back to a JSON string.
-        """
-        return json.dumps([item.to_dict() for item in self.items], indent=4)
+    def get_title(self) -> str:
+        return os.path.basename(self.filepath).replace('-', ' ').replace('_', ' ').replace('.md', '')
+
+    def __str__(self) -> str:
+        return self.get_title()
+
+    def get_github_url(self):
+        return f'{GIT_REPO}/tree/main/{self.filepath}'
+    
+    
+    def get_slug(self):
+        return self.filepath.replace('.md', '').replace('_', '-').replace(' ', '-')
+
+    def get_json(self):
+        return {
+        "github_url": self.get_github_url(),
+        "content": self.content,
+        "type": self.type.value,
+        "github_last_updated_date": self.last_updated,
+        "title": self.get_title(),
+        "slug_url": self.get_slug()
+    }
+
+    def get_post_json(self):
+        return {"data": self.get_json()}
+
+    def __eq__(self, __value) -> bool:
+        self.get_slug() == __value.get_slug()
--- blog/main.py
+++ blog/main.py
@@ -1,47 +1,122 @@
 import os
 import json
+import argparse
 from json.decoder import JSONDecodeError
 import requests
-from helpers import Item, ItemType, Items
+from urllib.parse import urljoin
+from helpers import Item, ItemType, StrapiBlog, StrapiBlogType
+from tqdm.auto import tqdm
+from datetime import datetime
+from pathlib import Path
 
-# load files.json into items
-items = Items()
-try:
-    items.load_from_file("files.json")
-except JSONDecodeError as e:
-    print('JSON Structure is invalid.')
-    exit(1)
-except Exception as e:
-    print('Unknown error occured.')
-    print(e)
-    exit(1)
+args = None
 
 BASE_URL = os.getenv('STRAPI_URL', "")
 API_KEY = os.getenv('STRAPI_API_KEY', "")
 
-session = requests.Session()
+paths_to_search = []
 
 headers = {
     'Authorization': f'Bearer {API_KEY}',
     'Content-Type': 'application/json'
 }
 
-# Example blog post data
-blog_post_data = {
-    "data": {
-        "github_url": "http://facebook.com",
-        "content": "This is a sample blog post created via API call."
-    }
-}
+def arg_parse():
+    global args
+    parser = argparse.ArgumentParser(description="VectorHub Strapi Upload")
+    parser.add_argument('--directories', help='Path to json which describes the directories to parse')
+    args = parser.parse_args()
 
-url = f"{BASE_URL}/api/blogs/4"
+def load_items_from_json(directories: str) -> list:
+    if os.path.exists(directories):
+        items = []
+        try:
+            with open(directories, 'r') as file:
+                data = json.load(file)
+                for item_data in data:
+                    items.append(Item.from_dict(item_data))
+        except JSONDecodeError as e:
+            print('JSON Structure is invalid.')
+            exit(1)
+        except Exception as e:
+            print('Unknown error occured.')
+            print(e)
+            exit(1)
+        return items
+    else:
+        print(f"{directories} does not exist.")
+        exit(1)
 
-# Send POST request to create a blog post
-create_response = session.put(
-    url, headers=headers, data=json.dumps(blog_post_data))
+def fetch_paths(node: Item, current_path=""):
+    global paths_to_search
+    # Update the current path with the node's path
+    current_path = f"{current_path}/{node.path}" if current_path else node.path
 
-# Check if the request was successful
-if create_response.status_code == 200:
-    print("Blog post created successfully")
-else:
-    print("Failed to create blog post:", create_response.text)
+    # If the node has children, recurse on each child
+    if node.has_blogs:
+        paths_to_search.append(current_path)
+    if node.children and len(node.children) > 0:
+        for child in node.children:
+            fetch_paths(child, current_path)
+    # else:
+        # If there are no children, print the current path
+        # print(current_path)
+
+def find_files_to_upload(items: list):
+    global paths_to_search
+
+    for item in items:
+        fetch_paths(item)
+
+    files = []
+
+    extension = 'md'
+    
+    for path in paths_to_search:
+        folder_path = Path(path)
+        folder_files = folder_path.glob(f"*.{extension}")
+        for file in folder_files:
+            if 'readme.md' not in str(file).lower():
+                files.append(str(file))
+
+    return files
+
+
+def build_blog_object(filepath: str) -> StrapiBlog:
+    with open(filepath, 'r') as file:
+        content = file.read()
+        blog = StrapiBlog(content, filepath, datetime.now().strftime("%Y-%m-%d"), StrapiBlogType.USECASE)
+        print(blog.get_slug())
+        return blog
+
+def upload_blog(blog: StrapiBlog):
+    base_url = urljoin(BASE_URL, 'api/blogs')
+    search_url = base_url + f"?filters[slug_url][$eq]={blog.get_slug()}"
+    session = requests.Session()
+
+    response = session.get(search_url, headers=headers)
+
+    if response.status_code == 200:
+        responses = json.loads(response.text)['data']
+
+        if len(responses) > 0:
+            # Blog already exists at this slug
+            id = json.loads(response.text)['data'][0]['id']
+
+            url = f"{base_url}/{id}"
+            create_response = session.put(url, headers=headers, data=json.dumps(blog.get_post_json()))
+        else:
+            # Its a new blog
+            url = base_url
+            create_response = session.post(url, headers=headers, data=json.dumps(blog.get_post_json()))
+
+
+if __name__ == "__main__":
+    arg_parse()
+    items = load_items_from_json(args.directories)
+
+    files = find_files_to_upload(items)
+
+    for file in tqdm(files):
+        blog = build_blog_object(file)
+        upload_blog(blog)
