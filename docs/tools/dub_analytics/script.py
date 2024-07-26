b32b35ccc61a05e5fa983630137b9a16207e861c
Chirag Jain <jainchg@gmail.com>
2024-07-27 01:48:34 +0530

    fetch data for last 7 days only

--- docs/tools/dub_analytics/script.py
+++ docs/tools/dub_analytics/script.py
@@ -4,6 +4,8 @@ import requests
 
 from tqdm.auto import tqdm
 
+from datetime import date
+
 from urllib.parse import urljoin
 
 DUB_CO_API_TOKEN = os.getenv("DUB_CO_API_TOKEN")
@@ -26,6 +28,11 @@ headers = {
     'Authorization': f'Bearer {DUB_CO_API_TOKEN}',
     'Content-Type': 'application/json'
 }
+
+def upload_zapier_hook_data(data, zap_id) -> None:
+    url = f"https://hooks.zapier.com/hooks/catch/{zap_id}/"
+    requests.post(url, json=data)
+
 
 def get_shortened_url_objs() -> list:
     global headers, DUB_CO_API_TOKEN, BASE_URL
@@ -56,6 +63,7 @@ def get_link_click_cnt(linkId) -> int:
 
     params = {
         'linkId': f'{linkId}',
+        'interval': '7d'
     }
 
     req = requests.get(urljoin(BASE_URL, "analytics"), headers=headers, params=params)
@@ -68,11 +76,15 @@ def get_link_click_cnt(linkId) -> int:
 def get_links_clicks(url_objs) -> list:
     print('Fetching click count for every URL', flush=True)
 
+    today = date.today()
+
     for obj in tqdm(url_objs):
         obj.set_click_cnt(get_link_click_cnt(obj.id))
-    
-    for obj in url_objs:
-        print(obj.count)
+        zap_data = {
+            "time": str(today),
+            f"{obj.id}": f"{obj.count}"
+        }
+        upload_zapier_hook_data(zap_data, "5388531/2urr55c")
 
 shortened_url_objs = get_shortened_url_objs()
 get_links_clicks(shortened_url_objs)
